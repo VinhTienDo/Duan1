6baa700523ceb4fe9a86741eee94b9a2da44821d
hoangphph35245 <hoangphph35249@fpt.edu.vn>
2023-11-15 18:12:56 +0700

    Initial commit

---
+++ app/src/main/java/fpoly/hoangphph35249/duan1/frg/fragemnt_add_user.java
@@ -0,0 +1,30 @@
+package fpoly.hoangphph35249.duan1.frg;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fpoly.hoangphph35249.duan1.R;
+
+
+public class fragemnt_add_user extends Fragment {
+
+
+
+    public fragemnt_add_user() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragemnt_add_user, container, false);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Duan1"
+include(":app")
+ 
