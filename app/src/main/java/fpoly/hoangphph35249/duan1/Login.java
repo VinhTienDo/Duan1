12fde8a242417ed6c3f50ae4ca9d265b63df9292
vinhldph35167 <vinhldph35167@fpt.edu.vn>
2023-11-18 07:55:51 +0700

    .

--- app/src/main/java/fpoly/hoangphph35249/duan1/Login.java
+++ app/src/main/java/fpoly/hoangphph35249/duan1/Login.java
@@ -3,6 +3,8 @@ package fpoly.hoangphph35249.duan1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText edUserName = findViewById(R.id.edUserName);
+        EditText edPassWord = findViewById(R.id.edPassword);
+        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnCancel = findViewById(R.id.btnCancel);
     }
 }
